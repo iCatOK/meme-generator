a541c2ece79935ef45eda4241403b4ff882d583e
iCatOK <kamilyen5@gmail.com>
2021-12-11 14:41:20 +0500

    таймер для успешности запроса

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 from pywebio.output import output, put_column, put_image, put_scrollable, put_text, toast, use_scope
 from pywebio.input import file_upload, input_group, input
 import uuid
-import os
+import time
 
 
 # стиль главного контейнера
@@ -18,8 +18,10 @@ all_center_container_css = """
 
 
 def validate(data):
+    start_time = time.time()
     open(f'asset/{uuid.uuid1()}_{data["source_1"]["filename"]}', 'wb').write(data['source_1']['content'])
     open(f'asset/{uuid.uuid1()}_{data["source_2"]["filename"]}', 'wb').write(data['source_2']['content'])
+    toast(f"Успешно: время - {time.time() - start_time} сек.")
 
 
 # домашнаяя страница
@@ -33,3 +35,4 @@ def home_page():
             input('Текст для сурса 1', name='text_1'),
             input('Текст для сурса 2', name='text_2')
         ], validate=validate)
+        
