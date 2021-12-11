8df8b1766bc0bafc4344957c8086fbe8ec4e9acd
iCatOK <kamilyen5@gmail.com>
2021-12-11 13:50:06 +0500

    наслаждайся жижей

--- home.py
+++ home.py
@@ -33,7 +33,3 @@ def home_page():
             input('Текст для сурса 1', name='text_1'),
             input('Текст для сурса 2', name='text_2')
         ], validate=validate)
-    
-    
-
-    put_text(f"{os.listdir('asset')}")
