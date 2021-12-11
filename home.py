0c050771023fd95ce77953b140eb65ffcb69c528
iCatOK <kamilyen5@gmail.com>
2021-12-11 13:03:24 +0500

    first commit

---
+++ home.py
@@ -0,0 +1,38 @@
+from pywebio.output import output, put_column, put_image, put_scrollable, put_text, toast, use_scope
+from pywebio.input import file_upload, input_group, input
+import uuid
+
+
+# стиль главного контейнера
+all_center_container_css = """
+    position: absolute;
+    top: 40%;
+    transform: translateY(-60%);
+    align-items: center;
+    align-self: center;
+    display: flex;
+    flex-direction: column;
+    width: 100vh;
+"""
+
+
+def validate(data):
+    open(f'asset/{uuid.uuid1()}_{data["source_1"]["filename"]}', 'wb').write(data['source_1']['content'])
+    open(f'asset/{uuid.uuid1()}_{data["source_2"]["filename"]}', 'wb').write(data['source_2']['content'])
+
+
+# домашнаяя страница
+def home_page():
+    container = output()
+    put_column(container).style(all_center_container_css)
+
+    input_group("Загрузите файлы для мема",[
+            file_upload("Сурс 1", name='source_1'),
+            file_upload("Сурс 2", name='source_2'),
+            input('Текст для сурса 1', name='text_1'),
+            input('Текст для сурса 2', name='text_2')
+        ], validate=validate)
+
+    for _ in range(2):
+        f1 = file_upload("Сурс 1")                  
+        open('asset/'+f1['filename'], 'wb').write(f1['content'])
---
+++ main.py
@@ -0,0 +1,24 @@
+from pywebio.platform import start_server
+from pywebio.session import go_app
+from home import home_page
+import argparse
+
+
+# главная страница сайта - редирект на страницу авторизации
+def index():
+    go_app('home_page', new_window=False)
+
+
+# основные модули сайта
+main_router = [
+    index,
+    home_page
+]
+
+
+# запуск сайта
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
+    start_server(main_router, port=args.port, debug=True)
